3526cd8b9a2218dafbdb54f33cf76e4617e268a5
Becca Jonas <beccajonas3@gmail.com>
2024-01-11 16:28:25 -0500

    solution

--- app.py
+++ app.py
@@ -15,14 +15,68 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-
 @app.get("/")
 def index():
     return "doctor/patient"
 
+@app.get('/doctors')
+def get_doctors():
+    doctors = Doctor.query.all()
+    return [d.to_dict(rules=['-appointments']) for d in doctors], 200
 
+@app.get('/doctors/<int:id>')
+def get_doctors_by_id(id):
+    doctor = Doctor.query.filter_by(id=id).first()
+    if not doctor:
+        return {'error': 'not found'}
+    return doctor.to_dict(rules=['-appointments.patient_id', '-appointments.doctor_id'])
 
+@app.get('/patients/<int:id>')
+def get_patient_by_id(id):
+    patient = Patient.query.filter_by(id=id).first()
+    patient_dict = patient.to_dict(rules=['-appointments'])
+    doctors = [d.to_dict(rules=['-appointments']) for d in patient.doctors]
+    patient_dict['doctors'] = doctors
+    return patient_dict
 
-
+@app.post('/doctors')
+def post_doctors():
+    try:
+        data = request.json
+        doctor = Doctor(name=data.get('name'), specialty=data.get('specialty'))
+        db.session.add(doctor)
+        db.session.commit()
+        return doctor.to_dict(), 201
+    except Exception as e:
+        return {"Error": str(e)}
+    
+@app.patch('/patients/<int:id>')
+def patch_patients(id):
+    # any time user input use try/catch
+    try:
+        data = request.json
+        patient = db.session.get(Patient, id)
+        if not patient:
+            return {"Error": "not foudn"}
+        for key in data:
+            setattr(patient, key, data[key])
+        db.session.add(patient)
+        db.session.commit()
+        return patient.to_dict(rules=["-appointments"])
+    
+    except Exception as e:
+        return {"Error": str(e)}
+    
+@app.post('/appointments')
+def post_appointments():
+    try:
+        data = request.json
+        appointment = Appointment(day=data.get('day'), patient_id=data.get('patient_id'), doctor_id=data.get('doctor_id'))
+        db.session.add(appointment)
+        db.session.commit()
+        return appointment.to_dict(rules=['-patient_id', '-doctor_id'])
+    except Exception as e:
+        return {"Error": str(e)}
+    
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
--- models.py
+++ models.py
@@ -10,22 +10,56 @@ metadata = MetaData(
         "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
     }
 )
+
 db = SQLAlchemy(metadata=metadata)
 
 
 class Patient(db.Model, SerializerMixin):
     __tablename__ = "patient_table"
-    id = db.Column(db.Integer, primary_key=True)
+    serialize_rules = ['-appointments.patient']
 
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    appointments = db.relationship("Appointment", back_populates="patient")
+
+    doctors = association_proxy('appointments', 'doctor')
 
 
 class Appointment(db.Model, SerializerMixin):
     __tablename__ = "appointment_table"
+    serialize_rules = ['-patient.appointments', '-doctor.appointments']
     id = db.Column(db.Integer, primary_key=True)
+    day = db.Column(db.String, nullable=False)
+    doctor_id = db.Column(db.Integer, db.ForeignKey("doctor_table.id"), nullable=False)
+    patient_id = db.Column(
+        db.Integer, db.ForeignKey("patient_table.id"), nullable=False
+    )
 
-
+    patient = db.relationship("Patient", back_populates="appointments")
+    doctor = db.relationship("Doctor", back_populates="appointments")
+    
+    @validates('day')
+    def validate_day(self, key, day):
+        weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+        if day not in weekdays:
+            raise ValueError("Doctors don't work on weekends here")
+        return day
 
 
 class Doctor(db.Model, SerializerMixin):
     __tablename__ = "doctor_table"
+    serialize_rules = ['-appointments.doctor']
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    specialty = db.Column(db.String, nullable=False)
+    appointments = db.relationship("Appointment", back_populates="doctor")
+    # name = validate_name("name", "Dr. Smith")
+    @validates("name")
+    def validate_name(self, key, name):
+        if not name.startswith("Dr."):
+            raise ValueError("Doctor name must start with Dr.")
+        return name
+    
+    patients = association_proxy('appointments', 'patient')
+
+
